7ae51c636f7d578e8318cfbf673bab2b0b391cb2
Pelonchas <pelonchas@Pelonchass-Mac-mini.local>
2026-01-22 12:35:10 -0700

    Layout test 3

--- src/pages/Learn.tsx
+++ src/pages/Learn.tsx
@@ -15,8 +15,8 @@ const Learn = () => (
       }}
     >
       <HiraganaLearnSection />
-      <KatakanaLearnSection />
-      <WordLearnSection />
+      {/* <KatakanaLearnSection /> */}
+      {/* <WordLearnSection /> */}
     </Card>
   </LearnContainer>
 );
